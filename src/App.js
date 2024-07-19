dc630fff00fe38d4e3f5dccaa13480ddebea2305
Devangana Deshmukh <91189547+devangana123@users.noreply.github.com>
2024-07-19 16:49:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import LoginSignUp from './components/LoginSignUp';
 import Campus from './components/Campus';
 function App() {
